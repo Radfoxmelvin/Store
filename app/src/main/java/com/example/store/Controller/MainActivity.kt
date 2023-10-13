0807af9c6fb6db4475d145e816cd74d82b4f2326
Radfoxmelvin <141583445+Radfoxmelvin@users.noreply.github.com>
2023-10-13 13:00:31 +0200

    online app part01

--- app/src/main/java/com/example/store/Controller/MainActivity.kt
+++ app/src/main/java/com/example/store/Controller/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.store
+package com.example.store.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.store.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
